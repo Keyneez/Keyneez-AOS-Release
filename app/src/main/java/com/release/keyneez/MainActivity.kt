cd0188afc1261c452c46dbeebef2d3c3472d0f04
b1urrrr <b1urrr@naver.com>
2023-03-22 22:31:09 +0900

    [ADD/#1] add initial project files

---
+++ app/src/main/java/com/release/keyneez/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.release.keyneez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
