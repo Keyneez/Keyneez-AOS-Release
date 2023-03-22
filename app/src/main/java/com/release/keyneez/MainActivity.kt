9e41ee96b3e0d4e882846899776537732fbe6f3a
b1urrrr <b1urrr@naver.com>
2023-03-23 03:58:46 +0900

    [ADD] gradle 설정 및 기본 의존성 추가

--- app/src/main/java/com/release/keyneez/MainActivity.kt
+++ app/src/main/java/com/release/keyneez/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.release.keyneez
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
