bb8217456bc8c9e09e8b3599138b6ccf2a65dde1
b1urrrr <b1urrr@naver.com>
2023-03-23 03:33:39 +0900

    [FEAT/#1] 카카오 로그인 의존성 추가 및 해시 값 출력 구현

--- app/src/main/java/com/release/keyneez/MainActivity.kt
+++ app/src/main/java/com/release/keyneez/MainActivity.kt
@@ -2,10 +2,15 @@ package com.release.keyneez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
     }
 }
