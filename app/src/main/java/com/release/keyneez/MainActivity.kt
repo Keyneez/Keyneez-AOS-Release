d5fb241e2309c4201ed644dd8b5cc43203b01ec8
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-03-30 15:48:57 +0900

    [MOD] mainactivity에서 첫 화면 수정 및 검색 뷰 일부 수정

--- app/src/main/java/com/release/keyneez/MainActivity.kt
+++ app/src/main/java/com/release/keyneez/MainActivity.kt
@@ -8,7 +8,7 @@ import com.kakao.sdk.common.util.Utility
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
 
         val keyHash = Utility.getKeyHash(this)
         Log.d("Hash", keyHash)
