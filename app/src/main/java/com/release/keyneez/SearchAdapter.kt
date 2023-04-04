122211ace4919a569416b544f876f072ced84489
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-04-04 16:13:13 +0900

    [ADD/#2] 머지를 위한 커밋

--- app/src/main/java/com/release/keyneez/SearchAdapter.kt
+++ app/src/main/java/com/release/keyneez/SearchAdapter.kt
@@ -1,4 +1,10 @@
 package com.release.keyneez
 
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class SearchAdapter {
+    lateinit var searchAdapter: SearchAdapter
+    private val viewModel by viewModels<SearchViewModel>()
+
 }
