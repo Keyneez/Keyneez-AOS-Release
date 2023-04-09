b8ca43e40eb8143f4d2aff794ab40aeefcf30025
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-04-09 12:55:39 +0900

    [FEAT/#3] 검색 버튼 기능 생성

--- app/src/main/java/com/release/keyneez/SearchActivity.kt
+++ app/src/main/java/com/release/keyneez/SearchActivity.kt
@@ -1,4 +1,29 @@
 package com.release.keyneez
 
-class SearchActivity {
+import android.os.Bundle
+import android.view.KeyEvent
+import com.release.keyneez.databinding.ActivitySearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchActivity : BindingActivity<ActivitySearchBinding>(R.layout.activity_search) {
+    lateinit var searchAdapter: SearchAdapter
+    private val viewModel by viewModels<SearchViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.vm = viewModel
+        initSearchBtnClickListener()
+    }
+
+    private fun initSearchBtnClickListener() {
+        binding.btnSearchResult.setOnKeyListener { v, keyCode, event ->
+            if ((event.action == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                viewModel.getSearchPostData()
+                true
+            } else {
+                false
+            }
+        }
+    }
 }
