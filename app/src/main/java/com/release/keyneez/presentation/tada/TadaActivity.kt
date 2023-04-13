d166980f12cacd71b77b448d1b71ad69dc03adc8
b1urrrr <b1urrr@naver.com>
2023-04-13 17:57:52 +0900

    [UI/#6] tada 레이아웃 구현

--- app/src/main/java/com/release/keyneez/presentation/tada/TadaActivity.kt
+++ app/src/main/java/com/release/keyneez/presentation/tada/TadaActivity.kt
@@ -1,13 +1,13 @@
-package com.release.keyneez.presentation.main
+package com.release.keyneez.presentation.tada
 
 import android.os.Bundle
 import com.release.keyneez.R
-import com.release.keyneez.databinding.ActivityMainBinding
+import com.release.keyneez.databinding.ActivityTadaBinding
 import com.release.keyneez.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+class TadaActivity : BindingActivity<ActivityTadaBinding>(R.layout.activity_tada) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
