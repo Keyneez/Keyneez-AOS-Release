c5fad79b35c0821d467854538946a4f4c976fa37
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-05-04 23:06:55 +0900

    [UI/#17] 탐색뷰 coordinatorlayout으로 해 보기

--- app/src/main/java/com/release/keyneez/presentation/main/MainActivity.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/MainActivity.kt
@@ -9,7 +9,7 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_explore)
 
         val keyHash = Utility.getKeyHash(this)
         Timber.tag("Hash").d(keyHash)
