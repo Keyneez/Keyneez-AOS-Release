e91a52d04ef4fcae123a9215eb08656c2fb5e9d6
tama <readyjoo24@gmail.com>
2023-05-09 00:59:22 +0900

    [UI/#23] 바텀시트 기본 레이아웃 생성

---
+++ app/src/main/java/com/release/keyneez/presentation/signup/SignupBottomSheetFragment.kt
@@ -0,0 +1,17 @@
+package com.release.keyneez.presentation.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class SignupBottomSheetFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
