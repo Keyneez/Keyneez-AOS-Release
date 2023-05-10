8c4ed7adc95825cbe313152b05e62a3b61795c70
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-05-11 00:45:46 +0900

    [MOD/#17] exploreAdapter return 수정

--- app/src/main/java/com/release/keyneez/presentation/main/explore/ExploreAdapter.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/explore/ExploreAdapter.kt
@@ -4,8 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.release.keyneez.presentation.main.explore.popular.PopularFragment
+import com.release.keyneez.presentation.main.explore.recent.RecentFragment
 
-class ExploreAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+class ExploreAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
         return 2
@@ -13,10 +15,9 @@ class ExploreAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle) :
 
     override fun createFragment(position: Int): Fragment {
         when (position) {
-            0 -> return
-            1 -> return
-            2 -> return
+            0 -> return PopularFragment()
+            1 -> return RecentFragment()
         }
-        return
+        return PopularFragment()
     }
 }
