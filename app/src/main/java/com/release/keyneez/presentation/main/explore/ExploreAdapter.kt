897234807e6b95e5e442973afc7d0cb59084df7e
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-05-15 01:02:47 +0900

    [MOD/#17] 코드 수정

--- app/src/main/java/com/release/keyneez/presentation/main/explore/ExploreAdapter.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/explore/ExploreAdapter.kt
@@ -6,13 +6,17 @@ import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.release.keyneez.presentation.main.explore.popular.PopularFragment
 import com.release.keyneez.presentation.main.explore.recent.RecentFragment
+import javax.annotation.Nonnull
 
 class ExploreAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
+    @Override
     override fun getItemCount(): Int {
         return 2
     }
 
+    @Override
+    @Nonnull
     override fun createFragment(position: Int): Fragment {
         when (position) {
             0 -> return PopularFragment()
