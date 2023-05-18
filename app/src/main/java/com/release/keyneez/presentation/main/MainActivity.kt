9b8266fc6988efb974342b8b764f445d1be59053
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-05-18 18:23:08 +0900

    [MOD/#17] 뷰데이터바인딩 관련하여 수정

--- app/src/main/java/com/release/keyneez/presentation/main/MainActivity.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.release.keyneez.presentation.main
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.common.util.Utility
 import com.release.keyneez.R
+import com.release.keyneez.databinding.ActivityMainBinding
+import com.release.keyneez.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_explore)
+        setContentView(binding.root)
 
         val keyHash = Utility.getKeyHash(this)
         Timber.tag("Hash").d(keyHash)
