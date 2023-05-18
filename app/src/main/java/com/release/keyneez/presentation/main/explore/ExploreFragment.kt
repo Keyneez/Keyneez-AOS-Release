7340187c37075be678969a5b6588052aae53e534
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-05-18 22:00:56 +0900

    [MOD/#17] 검색 버튼 누르면 검색 뷰로 이동하는 것 1차 작업

--- app/src/main/java/com/release/keyneez/presentation/main/explore/ExploreFragment.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/explore/ExploreFragment.kt
@@ -1,17 +1,21 @@
 package com.release.keyneez.presentation.main.explore
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.google.android.material.tabs.TabLayoutMediator
 import com.release.keyneez.R
 import com.release.keyneez.databinding.FragmentExploreBinding
+import com.release.keyneez.presentation.main.search.SearchActivity
 import com.release.keyneez.util.binding.BindingFragment
+import com.release.keyneez.util.extension.setOnSingleClickListener
 
 class ExploreFragment : BindingFragment<FragmentExploreBinding>(R.layout.fragment_explore) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initExploreViewPagerAdapter()
+        initSearchClickListener()
     }
 
     private fun initExploreViewPagerAdapter() {
@@ -28,6 +32,13 @@ class ExploreFragment : BindingFragment<FragmentExploreBinding>(R.layout.fragmen
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = exploreTabTitles[position]
         }.attach()
+    }
+
+    private fun initSearchClickListener() {
+        binding.btnExploreSearch.setOnSingleClickListener {
+            val intent = Intent(getActivity(), SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
