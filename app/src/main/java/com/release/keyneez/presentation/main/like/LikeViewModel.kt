b52bb6457f6c52be9bca341631d8ecf31a3b5658
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-06-18 16:31:40 +0900

    [ADD/#19] viewModel 추가

--- app/src/main/java/com/release/keyneez/presentation/main/like/LikeViewModel.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/like/LikeViewModel.kt
@@ -1,4 +1,55 @@
 package com.release.keyneez.presentation.main.like
 
-class LikeViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.release.keyneez.R
+import com.release.keyneez.domain.model.Activity
+
+class LikeViewModel : ViewModel() {
+    private val _activityList = MutableLiveData<List<Activity>>()
+    val activityList: LiveData<List<Activity>>
+        get() = _activityList
+
+    init {
+        getLikeActivityList()
+    }
+
+    private fun getLikeActivityList() {
+        val mainList = listOf(
+            Activity(
+                id = 1,
+                background = R.drawable.img_explore_background,
+                title = "행주산성\n맛집 투어",
+                category = "진로",
+                date = "%s-%s",
+                liked = true
+            ),
+            Activity(
+                id = 1,
+                background = R.drawable.img_explore_background,
+                title = "행주산성\n맛집 투어",
+                category = "진로",
+                date = "%s-%s",
+                liked = true
+            ),
+            Activity(
+                id = 1,
+                background = R.drawable.img_explore_background,
+                title = "행주산성\n맛집 투어",
+                category = "진로",
+                date = "%s-%s",
+                liked = true
+            ),
+            Activity(
+                id = 1,
+                background = R.drawable.img_explore_background,
+                title = "행주산성\n맛집 투어",
+                category = "진로",
+                date = "%s-%s",
+                liked = true
+            )
+        )
+        _activityList.value = mainList
+    }
 }
