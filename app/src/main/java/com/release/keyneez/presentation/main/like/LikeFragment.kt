1ac51733a87adb86929ccfc72fc546b587eaae9d
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-06-18 17:07:45 +0900

    [MOD/#19] 리사이클러뷰 dp값 추가

--- app/src/main/java/com/release/keyneez/presentation/main/like/LikeFragment.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/like/LikeFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.release.keyneez.R
 import com.release.keyneez.databinding.FragmentLikeBinding
+import com.release.keyneez.presentation.main.explore.popular.PopularFragment
 import com.release.keyneez.util.binding.BindingFragment
 import com.release.keyneez.util.extension.setOnSingleClickListener
 
@@ -15,6 +16,7 @@ class LikeFragment : BindingFragment<FragmentLikeBinding>(R.layout.fragment_like
         super.onViewCreated(view, savedInstanceState)
         initLikeAdapter()
         initCategoryBtnClickListener()
+        setupLikeActivityList()
     }
 
     private fun initLikeAdapter() {
@@ -27,5 +29,21 @@ class LikeFragment : BindingFragment<FragmentLikeBinding>(R.layout.fragment_like
         binding.tvLikeHobby.setOnSingleClickListener {}
         binding.tvLikeCareer.setOnSingleClickListener {}
         binding.tvLikeOutside.setOnSingleClickListener {}
+    }
+
+    private fun setupLikeActivityList() {
+        viewModel.activityList.observe(viewLifecycleOwner) { activityList ->
+            likeAdapter?.submitList(activityList)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        likeAdapter = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PopularFragment()
     }
 }
