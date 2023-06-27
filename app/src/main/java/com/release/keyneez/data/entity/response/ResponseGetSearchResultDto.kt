4347abf60049c37b92565f77b8dd900cec62a38f
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-06-27 22:02:29 +0900

    [MOD/#32] Dto 수정

--- app/src/main/java/com/release/keyneez/data/entity/response/ResponseGetSearchResultDto.kt
+++ app/src/main/java/com/release/keyneez/data/entity/response/ResponseGetSearchResultDto.kt
@@ -5,10 +5,18 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseGetSearchResultDto(
-    @SerialName("content_key") val contentKey: Int,
-    @SerialName("content_title") val contentTitle: String,
+    @SerialName("content_pk") val contentPk: Int,
+    @SerialName("title") val title: String,
+    @SerialName("category") val category: String,
+    @SerialName("img") val img: String?,
     @SerialName("start_at") val start: String?,
     @SerialName("end_at") val end: String?,
-    @SerialName("content_img") val contentImg: String?,
-    @SerialName("liked") val liked: Boolean
-)
+    val Likes: Liked?
+) {
+    @Serializable
+    data class Liked(
+        @SerialName("liked_pk") val likedPk: Int,
+        @SerialName("user") val user: Int,
+        @SerialName("content") val content: Int
+    )
+}
