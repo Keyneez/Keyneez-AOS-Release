a052c7de5041ad546ce706f6aed5d31d0c62a7fd
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-07-07 15:26:12 +0900

    [MOD/#33] selector 관련하여 코드 수정

--- app/src/main/java/com/release/keyneez/presentation/main/explore/popular/PopularFragment.kt
+++ app/src/main/java/com/release/keyneez/presentation/main/explore/popular/PopularFragment.kt
@@ -3,16 +3,15 @@ package com.release.keyneez.presentation.main.explore.popular
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
-import com.release.keyneez.R
 import com.release.keyneez.databinding.FragmentPopularBinding
 import com.release.keyneez.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class PopularFragment : BindingFragment<FragmentPopularBinding>(R.layout.fragment_popular) {
+class PopularFragment :
+    BindingFragment<FragmentPopularBinding>(com.release.keyneez.R.layout.fragment_popular) {
     private val viewModel: PopularViewModel by viewModels()
     private var popularAdapter: PopularAdapter? = null
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
